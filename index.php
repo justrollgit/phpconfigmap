723f63c65b07c56a6fd133a48fe629f74a04124d
justrollgit <45264247+justrollgit@users.noreply.github.com>
2019-02-21 07:46:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
+<style>
+<?php $ini_array = parse_ini_file("/config/app.properties");   ?>
+  body {background-color:  <?php print($ini_array['color']); ?> }
+</style>
+
+
 <?php
 
-$ini_array = parse_ini_file("/config/app.properties");
-
-print($ini_array['color']);
+Welcome to our little demo 
 ?>
